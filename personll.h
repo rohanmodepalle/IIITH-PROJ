27ce5940562c7cb4e0b2e598c0a3cf396070e325
harsha20032020 <nemani.v@research.iiit.ac.in>
2021-04-25 03:51:48 +0530

    added linked list implementations

---
+++ personll.h
@@ -0,0 +1,17 @@
+//#include "graph.c"
+
+struct person {
+    int date;
+    char string[10];
+    int source_station;
+    char destination;
+};
+
+struct tempstats{
+    int positive;
+    int primary;
+    int secondary;
+    int positive_array[100];
+    int primary_array[100];
+    int secondary_array[100];
+};
