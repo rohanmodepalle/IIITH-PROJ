c72505518118e294e4e2da478d5e650816c5aef3
harsha20032020 <nemani.v@research.iiit.ac.in>
2021-04-24 21:11:09 +0530

    implemented elems

--- person.h
+++ person.h
@@ -7,8 +7,17 @@ struct person {
     char destination;
 };
 
+struct element{
+    int data;
+    struct elemenet* next;
+};
 struct tempstats{
     int positive;
     int primary;
     int secondary;
+    struct element positive[100];
+    struct element primary[100];
+    struct element negative[100];
+
 };
+
